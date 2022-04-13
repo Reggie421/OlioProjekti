58d71b0302df4a0d4d09428bf5dea1b81115d5b7
Werneri <werneripunavaara@gmail.com>
2022-04-13 14:37:39 +0300

    Ui:ta kehitetty, Movie  info lisätty. Werneri

--- app/src/main/java/com/example/sdidebartesti/MovieInfoFragment.java
+++ app/src/main/java/com/example/sdidebartesti/MovieInfoFragment.java
@@ -11,7 +11,7 @@ import androidx.fragment.app.Fragment;
 public class MovieInfoFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_movieInfo, container, false);
+        return inflater.inflate(R.layout.fragment_movieinfo, container, false);
     }
 
     /*
