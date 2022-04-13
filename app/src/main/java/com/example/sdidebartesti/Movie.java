903232466cc0f6f9a5896f3530cb4a41432033f9
Reggie421 <wihtoripunavaara@icloud.com>
2022-04-13 15:55:26 +0300

    MovieManager lähes valmis T:pomo

--- app/src/main/java/com/example/sdidebartesti/Movie.java
+++ app/src/main/java/com/example/sdidebartesti/Movie.java
@@ -2,16 +2,19 @@ package com.example.sdidebartesti;
 
 public class Movie {
     int id;
-    int rating;
+    int year;
+    float rating;
     String title;
 
-    public Movie(int idValue, String movieName, int ratingValue){
+    public Movie(int idValue, String movieName, float ratingValue, int yearValue){
         id = idValue;
         title = movieName;
         rating = ratingValue;
+        year = yearValue;
 
     }
     public int getId(){return id;}
     public String getTitle(){return title;}
-    public int getRating(){return rating;}
+    public float getRating(){return rating;}
+    public int getYear(){return year;}
 }
