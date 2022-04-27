8d0ce691325ba3e1f025da07c29639e801aa3adb
meemimeisseli <joel.leppanen@hotmail.com>
2022-04-27 19:31:27 +0300

    kirjaudu ulos nappi toimii. joel

--- app/src/main/java/com/example/sdidebartesti/SettingsFragment.java
+++ app/src/main/java/com/example/sdidebartesti/SettingsFragment.java
@@ -1,10 +1,13 @@
 package com.example.sdidebartesti;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 import androidx.annotation.NonNull;
@@ -12,7 +15,8 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class SettingsFragment extends Fragment {
-
+    Button logout;
+    Button deleteAcc;
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_settings, container, false);
@@ -22,6 +26,25 @@ public class SettingsFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        logout = (Button) view.findViewById(R.id.logout);
+        deleteAcc = (Button) view.findViewById(R.id.deleteaccount);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Kirjaudutaan ulos");
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+
+        deleteAcc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("Poistetaan käyttäjä");
+            }
+        });
 
     }
 }
